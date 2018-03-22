162dd5f2e3e5063b5e7d477133bd58e33cd8ed4e
tzz <zzt1201@gmail.com>
2018-03-22 16:16:11 +0800

    Gui

--- FIR.py
+++ FIR.py
@@ -3,13 +3,6 @@ from Check import check
 from Analyse import depth_analyse
 
 
-class CoordinateError(Exception):
-    def __init__(self, info):
-        self.info = "YOU" if info == Chess.person_chess_pieces else "COMPUTER"
-    def __str__(self):
-        return self.info
-
-
 class Chess:
     person_chess_pieces = 1
     computer_chess_pieces = -1
@@ -17,18 +10,20 @@ class Chess:
     def __init__(self):
         self.chess_board = np.zeros((15, 15), dtype=np.int8)
         self.score_board = np.zeros((15, 15), dtype=np.int32)
-        self.first_player = None
+        self.first_player = np.random.choice([Chess.person_chess_pieces,
+                                              Chess.computer_chess_pieces])
         self.winner = None
 
     def put_chess(self, identity, coordinate_x, coordinate_y):
         # raise an error if user's input coordinate has already been taken
         if self.chess_board[coordinate_x][coordinate_y] != 0:
-            raise CoordinateError(self.chess_board[coordinate_x][coordinate_y])
+            return True
         # otherwise put the chess
         if identity == self.person_chess_pieces:
             self.chess_board[coordinate_x][coordinate_y] = self.person_chess_pieces
         elif identity == self.computer_chess_pieces:
             self.chess_board[coordinate_x][coordinate_y] = self.computer_chess_pieces
+        return False
 
     def analyse_put(self):
         # initialize score board
@@ -36,50 +31,8 @@ class Chess:
         # analyse the chess board and get the location
         coordinate = depth_analyse(self.chess_board, self.score_board, 
                                    np.random.choice([4, 6]))
-        # put the chess
-        self.put_chess(self.computer_chess_pieces, coordinate[1], coordinate[2])
-        # show it to the user
-        print("\nComputer at:", coordinate[1], coordinate[2], "\n")
+        return coordinate[1], coordinate[2]
 
     def check_winner(self):
         return check(self.chess_board)
 
-
-def main():
-    chess = Chess()
-    chess.first_player = np.random.choice([chess.person_chess_pieces,
-                                           chess.computer_chess_pieces])
-    print('%s' % ('You First'
-                  if chess.first_player == chess.person_chess_pieces
-                  else 'Computer First'))
-
-    if chess.first_player == chess.computer_chess_pieces:
-        chess.put_chess(chess.computer_chess_pieces, 7, 7)
-        print("\nComputer at: 7 7\n")
-
-    # main loop
-    while True:
-        # user's move
-        coordinate = [int(i) for i in filter(lambda x: x.isdigit(), input("Row and Column: (seperate by space)\n").split())]
-        try:
-            chess.put_chess(chess.person_chess_pieces, coordinate[0], coordinate[1])
-        except IndexError:
-            print("Invalid input, please try again")
-            continue
-        except CoordinateError as err:
-            print(f"This coordinate has been taken by {err}, please try again")
-            continue
-        if chess.check_winner():
-            print("Winner is YOU")
-            break
-
-        # computer's move
-        chess.analyse_put()
-
-        if chess.check_winner():
-            print("Winner is COMPUTER")
-            break
-
-
-if __name__ == '__main__':
-    main()
---
+++ FIRgui.py
@@ -0,0 +1,67 @@
+import math, time
+import tkinter as tk
+from FIR import Chess
+
+class FIRenv(tk.Tk):
+    def __init__(self):
+        # set up gui
+        super().__init__()
+        self.title('FIR GAME')
+        self.geometry('630x630')
+        self.__creat_canvas()
+        self.__setup_chess_board()
+        # set up core
+        self.chess = Chess()
+        if self.chess.first_player == self.chess.computer_chess_pieces:
+            self.__put_circle(7, 7)
+
+    
+    def __creat_canvas(self):
+        self.canvas = tk.Canvas(self, bg='lightcyan', width=630, height=630)
+        self.canvas.pack()
+
+    def __setup_chess_board(self):
+        for i in range(42, 630, 42):
+            # vertical line
+            self.canvas.create_line(i, 0, i, 630)
+            # horizontal line
+            self.canvas.create_line(0, i, 630, i)
+        # bind events
+        self.canvas.bind('<Button-1>', self.__scheduler)
+        # self.canvas.bind('<Button-2>', self.__regret)
+
+    def __put_circle(self, x, y):
+        print('computer', x, y)
+        self.chess.put_chess(Chess.computer_chess_pieces, x, y)
+        i = x * 42
+        j = y * 42
+        self.canvas.create_oval(i+4, j+4, i+38, j+38, width=3, outline='#e59832')
+        self.update()
+
+    def __put_cross(self, event):
+        i = math.floor(event.x/42)
+        j = math.floor(event.y/42)
+        print('me', i, j)
+        if self.chess.put_chess(Chess.person_chess_pieces, i, j):
+            return True
+        i *= 42
+        j *= 42
+        self.canvas.create_line(i+5, j+5, i+37, j+37, width=3, fill='#2585e5')
+        self.canvas.create_line(i+37, j+5, i+5, j+37, width=3, fill='#2585e5')
+        self.update()
+
+    def __scheduler(self, event):
+        if self.__put_cross(event):
+            return True
+        self.__put_circle(*self.chess.analyse_put())
+
+    # def __regret(self):
+    #     if self.core.history != []:
+    #         self.core.chess_board[self.core.history.pop()] = 0
+    #         self.canvas.delete(str(self.core.counter)+'_')
+    #         self.core.counter -= 1
+
+
+if __name__ == '__main__':
+    chess = FIRenv()
+    chess.mainloop()
